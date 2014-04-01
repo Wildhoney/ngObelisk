21ab849feae55e27f40b4d7079ace86fbed2e974
Adam Timberlake <adam.timberlake@gmail.com>
2014-04-01 14:38:19 +0100

    pwd

--- example/js/directives/Obelisk.js
+++ example/js/directives/Obelisk.js
@@ -3,7 +3,7 @@
     /**
      * @directive Obelisk
      */
-    $app.directive('obelisk', function obeliskController() {
+    $app.directive('obelisk', ['$window', 'obelisk', function obeliskController($window, obelisk) {
 
         return {
 
@@ -21,17 +21,17 @@
              */
             link: function link(scope, element) {
 
-                var point = new obelisk.Point(100, 100);
-                var pixelView = new obelisk.PixelView(element[0], point);
-                var dimension = new obelisk.CubeDimension(10, 10, 10);
-                var gray = obelisk.ColorPattern.GRAY;
-                var color = new obelisk.CubeColor().getByHorizontalColor(gray);
-                var cube = new obelisk.Cube(dimension, color, true);
+                var point = new $window.obelisk.Point(100, 100);
+                var pixelView = new $window.obelisk.PixelView(element[0], point);
+                var dimension = new $window.obelisk.CubeDimension(10, 10, 10);
+                var gray = $window.obelisk.ColorPattern.GRAY;
+                var color = new $window.obelisk.CubeColor().getByHorizontalColor(gray);
+                var cube = new $window.obelisk.Cube(dimension, color, true);
                 pixelView.renderObject(cube);
 
             }
         }
 
-    });
+    }]);
 
 })(window.obeliskApp);
--- module/ngObelisk.js
+++ module/ngObelisk.js
@@ -1,6 +1,20 @@
+/**
+ * @module ngObelisk
+ * @author Adam Timberlake
+ * @link http://github.com/Wildhoney/ngObelisk
+ */
 (function startObelisk($angular) {
 
     // Bootstrap ngObelisk, my lovelies!
     var app = $angular.module('ngObelisk', []);
+
+    /**
+     * @service obelisk
+     */
+    app.service('obelisk', function obeliskService() {
+
+        return {};
+
+    });
 
 })(window.angular);
