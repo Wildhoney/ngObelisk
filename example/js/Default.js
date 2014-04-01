549e1b9aed85b7f402df342683c0361478a7bee7
Adam Timberlake <adam.timberlake@gmail.com>
2014-04-01 14:34:25 +0100

    Bootstrapped ngObeliskc

--- example/js/Default.js
+++ example/js/Default.js
@@ -1,6 +1,6 @@
 (function startApplication($window) {
 
     // Bootstrap our Angular app!
-    $window.obeliskApp = $window.angular.module('obeliskApp', []);
+    $window.obeliskApp = $window.angular.module('obeliskApp', ['ngObelisk']);
 
 })(window);
---
+++ module/ngObelisk.js
@@ -0,0 +1,6 @@
+(function startObelisk($angular) {
+
+    // Bootstrap ngObelisk, my lovelies!
+    var app = $angular.module('ngObelisk', []);
+
+})(window.angular);
