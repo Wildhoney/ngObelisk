c93cd193ab81a9a09ad5ad63d2428ecd4f678f7d
Adam Timberlake <adam.timberlake@gmail.com>
2014-04-01 14:31:30 +0100

    Added initial drawing with Obelisk

---
+++ example/js/directives/Obelisk.js
@@ -0,0 +1,37 @@
+(function startObeliskDirective($app) {
+
+    /**
+     * @directive Obelisk
+     */
+    $app.directive('obelisk', function obeliskController() {
+
+        return {
+
+            /**
+             * @property restrict
+             * @type {String}
+             */
+            restrict: 'EA',
+
+            /**
+             * @method link
+             * @param scope {Object}
+             * @param element {Object}
+             * @return {void}
+             */
+            link: function link(scope, element) {
+
+                var point = new obelisk.Point(100, 100);
+                var pixelView = new obelisk.PixelView(element[0], point);
+                var dimension = new obelisk.CubeDimension(10, 10, 10);
+                var gray = obelisk.ColorPattern.GRAY;
+                var color = new obelisk.CubeColor().getByHorizontalColor(gray);
+                var cube = new obelisk.Cube(dimension, color, true);
+                pixelView.renderObject(cube);
+
+            }
+        }
+
+    });
+
+})(window.obeliskApp);
