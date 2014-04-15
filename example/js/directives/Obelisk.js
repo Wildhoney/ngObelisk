c539f2c60451f092302ed06eac624f9be61d9382
Adam Timberlake <adam.timberlake@gmail.com>
2014-04-15 22:36:38 +0100

    Added SideX and SideY objects. Fixes #3

--- example/js/directives/Obelisk.js
+++ example/js/directives/Obelisk.js
@@ -44,10 +44,10 @@
                  */
                 $scope.createCube = function createCube() {
 
-                    var dimension   = new obelisk.CubeDimension(20, 20, 50),
-                        gray        = obelisk.ColorPattern.GRAY,
-                        color       = new obelisk.CubeColor().getByHorizontalColor(gray),
-                        p3d         = new obelisk.Point3D(140, 40, 40);
+                    var dimension = new obelisk.CubeDimension(20, 20, 50),
+                        gray      = obelisk.ColorPattern.GRAY,
+                        color     = new obelisk.CubeColor().getByHorizontalColor(gray),
+                        p3d       = new obelisk.Point3D(140, 40, 40);
 
                     return $scope.pixelView.renderObject(new obelisk.Cube(dimension, color, true), p3d);
 
@@ -59,9 +59,9 @@
                  */
                 $scope.createPyramid = function createPyramid() {
 
-                    var dimension   = new obelisk.PyramidDimension(20),
-                        color       = new obelisk.PyramidColor().getByRightColor(obelisk.ColorPattern.GRASS_GREEN),
-                        p3d         = new obelisk.Point3D(140, 40, 90);
+                    var dimension = new obelisk.PyramidDimension(20),
+                        color     = new obelisk.PyramidColor().getByRightColor(obelisk.ColorPattern.GRASS_GREEN),
+                        p3d       = new obelisk.Point3D(140, 40, 90);
 
                     return $scope.pixelView.renderObject(new obelisk.Pyramid(dimension, color, true), p3d);
 
@@ -73,12 +73,39 @@
                  */
                 $scope.createBrick = function createBrick() {
 
-                    var dimension   = new obelisk.BrickDimension(100, 100),
-                        color       = new obelisk.SideColor().getByInnerColor(obelisk.ColorPattern.GRAY),
-                        p3d         = new obelisk.Point3D(130, 40, 90);
+                    var dimension = new obelisk.BrickDimension(100, 100),
+                        color     = new obelisk.SideColor().getByInnerColor(obelisk.ColorPattern.GRAY),
+                        p3d       = new obelisk.Point3D(130, 40, 90);
 
                     return $scope.pixelView.renderObject(new obelisk.Brick(dimension, color), p3d);
 
+                };
+
+                /**
+                 * @method createSideX
+                 * @return {Object}
+                 */
+                $scope.createSideX = function createSideX() {
+
+                    var colour    = new obelisk.SideColor().getByInnerColor(obelisk.ColorPattern.GRAY),
+                        dimension = new obelisk.SideXDimension(100, 10),
+                        p3d       = new obelisk.Point3D(130, 40, 90);
+
+                    return $scope.pixelView.renderObject(new obelisk.SideX(dimension, colour), p3d);
+                    
+                };
+
+                /**
+                 * @method createSideY
+                 * @return {Object}
+                 */
+                $scope.createSideY = function createSideY() {
+                    
+                    var colour    = new obelisk.SideColor().getByInnerColor(obelisk.ColorPattern.GRAY),
+                        dimension = new obelisk.SideYDimension(100, 10),
+                        p3d       = new obelisk.Point3D(130, 40, 90);
+                    
+                    return $scope.pixelView.renderObject(new obelisk.SideY(dimension, colour), p3d);
                 };
 
             },
@@ -95,6 +122,8 @@
                 scope.pixelView = new obelisk.PixelView(element[0], new obelisk.Point(250, 150));
 
                 var size    = 40,
+                    sideX   = scope.createSideX(),
+                    sideY   = scope.createSideY(),
                     brick   = scope.createBrick(),
                     cube    = scope.createCube(),
                     pyramid = scope.createPyramid();
@@ -110,8 +139,12 @@
 
                     cube.setX(size);
                     cube.setY(size);
+
                     brick.setX(100 + size);
                     brick.setY(100 + size);
+
+                    sideX.setX(100 + size);
+                    sideY.setY(100 + size);
 
                     pyramid.setX(size);
                     pyramid.setY(size);
