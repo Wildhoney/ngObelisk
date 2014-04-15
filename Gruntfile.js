ebe3576308860faf7dbce243db7062cf60249c02
Adam Timberlake <adam.timberlake@gmail.com>
2014-04-15 22:38:21 +0100

    Added compress for releases

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,27 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
 
+        /**
+         * @property pkg
+         * @type {Object}
+         */
         pkg: grunt.file.readJSON('package.json'),
+
+        /**
+         * @property jshint
+         * @type {Object}
+         */
         jshint: {
             all: 'module/ngObelisk.js',
             options: {
                 jshintrc: '.jshintrc'
             }
         },
+
+        /**
+         * @property uglify
+         * @type {Object}
+         */
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> by <%= pkg.author %> created on <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -18,6 +32,11 @@ module.exports = function(grunt) {
                 dest: 'dist/<%= pkg.name %>.min.js'
             }
         },
+
+        /**
+         * @property copy
+         * @type {Object}
+         */
         copy: {
             main: {
                 files: [
@@ -29,10 +48,30 @@ module.exports = function(grunt) {
                 dest: 'example/js/ng-obelisk.js'
             }
         },
+
+        /**
+         * @property rename
+         * @type {Object}
+         */
         rename: {
             main: {
                 files: [
                     {src: ['dist/ng-obelisk.js'], dest: 'dist/<%= pkg.name %>.js'}
+                ]
+            }
+        },
+
+        /**
+         * @property compress
+         * @type {Object}
+         */
+        compress: {
+            main: {
+                options: {
+                    archive: 'releases/<%= pkg.version %>.zip'
+                },
+                files: [
+                    { flatten: true, src: ['dist/**'], dest: 'releases/', filter: 'isFile' }
                 ]
             }
         }
@@ -41,9 +80,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
-    grunt.registerTask('build', ['copy', 'uglify']);
+    grunt.registerTask('build', ['copy', 'uglify', 'compress']);
     grunt.registerTask('test', ['jshint']);
-    grunt.registerTask('default', ['jshint', 'copy', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'copy', 'uglify', 'compress']);
 
 };
